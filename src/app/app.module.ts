c01d415d41fe2c52d784d1164709f0b7d54d930c
Ray Salvo <salvo.raymond@gmail.com>
2025-01-25 08:45:00 -0500

    First attempt at adding module - AppModule.

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule {}
